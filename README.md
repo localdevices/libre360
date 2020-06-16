d8e89b61cedafe2a8cea65fc54203a7941d2bed4
hcwinsemius <h.c.winsemius@gmail.com>
2020-06-16 11:55:06 +0200

    Prepared an environment #4

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # odm360
 
 This repository contains code for the EECS 398 group working on the odm360 project with the Cleveland Metroparks.
+
+To install an environment, use Miniconda and prepare an environment with
+```
+conda env create -f environment.yml
+``` 
+Before installation, make sure you have the gphoto2 library installed with
+```
+sudo apt install libgphoto2-dev
+```
+Then install odm360 as developer with
+```
+conda activate odm360
+pip install -e .
+```
---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="odm360",
+    description="odm360 is a python package to control a 360 camera uBlox positioning setup",
+    long_description=readme + "\n\n",
+    url="https://github.com/OpenDroneMap/odm360",
+    author="Stephen Mather",
+    author_email="svm@clevelandmetroparks.com",
+    packages=find_packages(),
+    package_dir={"odm360": "odm360"},
+    test_suite="tests",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    python_requires=">=3.6",
+    install_requires=[
+        "pySerial",
+        "numpy",
+        "scipy",
+        "gphoto2",
+        
+    ],
+    extras_require={
+        "dev": ["pytest", "pytest-cov", "black"],
+        "optional": [],
+    },
+    entry_points="""
+    """,
+    include_package_data=True,
+    license="???",
+    zip_safe=False,
+    classifiers=[
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Photogrammetry",
+        "License :: OSI Approved :: ???",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="opendronemap topography photogrammetry data-science odm360",
+)
