6e44708d2132a331ac0dabc9080ea81e498bd177
ivangayton <ivangayton@gmail.com>
2020-07-24 11:55:09 +0900

    Cleanup of .ubx reader

--- provisioning/read_serial_rawx_for_ppp.py
+++ provisioning/read_serial_rawx_for_ppp.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
 Read serial raw GNSS data from a device to a file.
-One positional argument: output file to write.
-Two flag arguments:
--br or --baud_rate: the expected baud rate of the device. Default 38400.
--to or --timeout: the time in seconds to wait before giving up on a port
--p or --port: the specific name of the port the device is on (don't search)
-Checks for serial ports, iterates through them and grabs the first one that
-provides a valid NMEA sentence. Then writes all following NMEA sentences
-to the specified output file.
+
+One positional argument: 
+output file to write.
+
+Flag arguments:
+-br or --baud_rate: expected baud rate of the device. Default 38400.
+-to or --timeout: time in seconds to wait before giving up on a port
+-p or --port: name of the port the device is on (don't search)
 """
 import sys, os
 import argparse
@@ -16,7 +16,7 @@ import argparse
 import serial
 import serial.tools.list_ports
 
-def make_serial_reader(baud_rate, timeout, port = None):
+def make_serial_reader(port = None, baud_rate, timeout):
     """Create a serial reader for a GNSS receiver."""
     if port:
         serial_reader = serial.Serial(port = "/dev/ttyS0",
@@ -26,14 +26,7 @@ def make_serial_reader(baud_rate, timeout, port = None):
                                       parity = serial.PARITY_NONE,
                                       stopbits = serial.STOPBITS_ONE)
         return serial_reader
-    ports = serial.tools.list_ports.comports()
-    for port in ports:
-        serial_reader = serial.Serial(port[0], baud_rate, timeout)
-        for i in range(10):
-            if(validate_sentence(serial_reader.readline())):
-                return serial_reader
-        return None
-
+    
 def log_raw_GNSS(serial_reader, outfile):
     """Write binary data from a serial port to a file"""
     with open(outfile, 'wb') as of:
@@ -44,23 +37,23 @@ def log_raw_GNSS(serial_reader, outfile):
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
-    p.add_argument('outfile', help = "Text file to write GNSS data to")
+    p.add_argument('outfile', help = "Text file to write data to")
     p.add_argument('-br', '--baud_rate', default = 460800,
                    help = 'expected baud rate of the device')
     p.add_argument('-to', '--timeout', default = 5,
-                   help = 'Time in seconds to wait before giving up on a port')
+                   help = 'Seconds to wait before giving up on port')
     p.add_argument('-p', '--port', default = None,
-                   help = 'Known serial port of device. Searches if omitted.')
+                   help = 'Known serial port of device.')
 
     opts = vars(p.parse_args())
-
+    
+    pt = opts['port']
     br = int(opts['baud_rate'])
     to = int(opts['timeout'])
-    pt = opts['port']
     of = opts['outfile']
     print(f'\nAttempting to create serial reader with:'
           '\nbaud rate: {br}'
           f'\non port {pt}')
-    reader = make_serial_reader(br,to, pt)
+    reader = make_serial_reader(pt, br,to)
     print(f'Writing output to {of}')
     log_raw_GNSS(reader, of)
