9490e95f3b520078c740c0f065acd26a8c21244a
ivangayton <ivangayton@gmail.com>
2020-09-13 23:11:45 +0900

    Script now correctly creates a postgres user (role) with hashed password and corresponding empty database

--- provisioning/database_setup.sh
+++ provisioning/database_setup.sh
@@ -8,7 +8,13 @@ sudo apt install -y postgresql postgresql-contrib libpq-dev
 # echo Installing requirements from setup.py using pip
 # pip3 install -e .
 
-sudo -u postgres createuser -d odm360
+echo Creating postgres user odm360
+sudo -u postgres psql -c "CREATE USER odm360 WITH PASSWORD 'md51bb536130df443a4bb77eaf446bca7ca';"
+
+echo Creating database odm360
+sudo -u postgres psql -c "CREATE DATABASE odm360 WITH OWNER odm360;"
+
+
 
 
 echo ##########################################################
