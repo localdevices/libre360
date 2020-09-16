b491a5d9c3471e63ede41e1eeddbf30067cdf8ce
Stephen Mather <1174901+smathermather@users.noreply.github.com>
2020-09-16 14:49:37 -0400

    Refine photos and device_status

--- examples/dbase.sql
+++ examples/dbase.sql
@@ -3,7 +3,7 @@
 DROP TABLE IF EXISTS projects CASCADE;
 CREATE TABLE IF NOT EXISTS projects
 (
-	projectid INT GENERATED ALWAYS AS IDENTITY -- rather than serial, we will comply with the SQL standard
+	projectid BIGINT GENERATED ALWAYS AS IDENTITY -- rather than serial, we will comply with the SQL standard
 	,projectname text NOT NULL -- I assuming this is a project name and have renamed as such
 	,root varchar (100) NOT NULL
 	,n_cams integer NOT NULL
@@ -15,13 +15,12 @@ CREATE TABLE IF NOT EXISTS projects
 DROP TABLE IF EXISTS photos;
 CREATE TABLE IF NOT EXISTS photos 
 (
-	photoid INT GENERATED ALWAYS AS IDENTITY  --renamed: UUID usually is usually a universally unique identifier, and this is a serial
+	photoid BIGINT GENERATED ALWAYS AS IDENTITY  --renamed: UUID usually is usually a universally unique identifier, and this is a serial
 	--,project varchar (50) NOT NULL -- Removing: this only belongs in the project file and will be joined using a foreign key
 	,projectid INT
 	,survey_run VARCHAR (50) NOT NULL
 	,device VARCHAR (50) NOT NULL
 	,photo_filename VARCHAR (100) NOT NULL
-	,photo_id VARCHAR (50) NOT NULL
 	,photo BYTEA NOT NULL
 	,thumbnail BYTEA
 	,PRIMARY KEY(photoid)
@@ -31,10 +30,15 @@ CREATE TABLE IF NOT EXISTS photos
 	   ON DELETE CASCADE
 );
 	
-
+DROP TABLE IF EXISTS device_status;
 CREATE TABLE IF NOT EXISTS device_status(
-	deviceid 
-	,devicename varchar (50) NOT NULL
-	, status integer NOT NULL
-	, last_photo varchar (100)
+	deviceid BIGINT GENERATED ALWAYS AS IDENTITY
+	,devicename text NOT NULL
+	,status integer NOT NULL
+	,last_photo photoid
+	,PRIMARY KEY(deviceid)
+	,CONSTRAINT fk_project -- add foreign key constraint referencing the project ID
+         FOREIGN KEY(photoid) 
+    	   REFERENCES photos(photoid)
+	   ON DELETE CASCADE	
 	);
