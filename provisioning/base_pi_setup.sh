e3198c65b342342910bff093c8848cd5a06d8719
ivangayton <ivangayton@gmail.com>
2020-09-17 12:50:52 +0900

    Base setup script checks if it is on a Pi

--- provisioning/base_pi_setup.sh
+++ provisioning/base_pi_setup.sh
@@ -3,6 +3,15 @@
 # Set up a Raspberry Pi with the basic infrastructure used by ODM360
 # Normally followed by a second script which sets up either a Parent or Child
 
+echo checking if this is running on a Raspberry Pi
+model="$( cat /proc/device-tree/model )"
+onpi="no"
+if [[ "$model" == *"Raspberry Pi"* ]]; then
+    echo "This is actually a Raspberry Pi!"
+    onpi="yes"
+fi
+   
+
 echo updating
 sudo apt update && sudo apt upgrade -y
 
@@ -12,24 +21,27 @@ sudo apt install -y git python3-pip libgphoto2-dev libatlas-base-dev gfortran
 echo Installing emacs because it is the best editor and IDE. You are welcome.
 sudo apt install -y emacs-nox
 
-echo Installing posgresql
+echo Installing postgresql
 sudo apt install -y postgresql postgresql-contrib libpq-dev
-
-# TODO check if already done before sedding in the disable flag
-echo Disabling IPv6
-sudo sed -i '$s/$/ ipv6.disable=1/' /boot/cmdline.txt
-
-echo Disabling serial console to free up UART serial line
-sudo sed -i 's/console=serial0,115200 //g' /boot/cmdline.txt
-
-# TODO check if already done before pushing this line into the file
-echo enabling UART
-echo $'\n# Enable UART\nenable_uart=1' | sudo tee -a /boot/config.txt
 
 echo Installing requirements from setup.py using pip
 pip3 install -e .
 
+# Check if on RPi before doing stuff specific to Pi
+if [[ onpi == "yes" ]]; then
+    # TODO check if already done before sedding in the disable flag
+    echo Disabling IPv6
+    sudo sed -i '$s/$/ ipv6.disable=1/' /boot/cmdline.txt
+    
+    echo Disabling serial console to free up UART serial line
+    sudo sed -i 's/console=serial0,115200 //g' /boot/cmdline.txt
+    
+    # TODO check if already done before pushing this line into the file
+    echo enabling UART
+    echo $'\n# Enable UART\nenable_uart=1' | sudo tee -a /boot/config.txt
+fi
+
 echo ##########################################################
-echo Now you should have a Raspberry Pi set up with the basic infrastructure common to all devices in the kit. The next steps depend whether this device is intended to be a Parent or Child.
+echo Now you should have a $model set up with the basic infrastructure common to all devices in the kit. The next steps depend whether this device is intended to be a Parent or Child.
 echo ##########################################################
 echo
