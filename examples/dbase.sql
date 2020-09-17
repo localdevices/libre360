7d0af10c6a4ace419a8579ee80a7f2cc5c789edd
Stephen Mather <1174901+smathermather@users.noreply.github.com>
2020-09-17 12:59:39 -0400

    fix so that photos constrained to FK device ID

--- examples/dbase.sql
+++ examples/dbase.sql
@@ -25,27 +25,6 @@ CREATE TABLE IF NOT EXISTS projectactive
 	  ON DELETE CASCADE	
 );
 
--- References the project and has a delete cascade, so if the project gets deleted, the photos get deleted
--- Yay! Tidy databases
-DROP TABLE IF EXISTS photos CASCADE;
-CREATE TABLE IF NOT EXISTS photos 
-(
-	photouuid uuid DEFAULT uuid_generate_v4 ()
-	--,project varchar (50) NOT NULL -- Removing: this only belongs in the project file and will be joined using a foreign key
-	,projectid INT
-	,survey_run text NOT NULL
-	,device text NOT NULL
-	,photo_filename text NOT NULL
-	,photo BYTEA NOT NULL
-	,thumbnail BYTEA
-	,PRIMARY KEY(photouuid)
-	,CONSTRAINT fk_project -- add foreign key constraint referencing the project ID
-         FOREIGN KEY(projectid) 
-    	   REFERENCES projects(projectid)
-	   ON DELETE CASCADE
-);
-
-
 DROP TABLE IF EXISTS devices CASCADE;
 CREATE TABLE IF NOT EXISTS devices
 (
@@ -59,3 +38,29 @@ CREATE TABLE IF NOT EXISTS devices
     	   REFERENCES photos(photouuid)
 	   ON DELETE CASCADE	
 );
+
+-- References the project and has a delete cascade, so if the project gets deleted, the photos get deleted
+-- Yay! Tidy databases
+DROP TABLE IF EXISTS photos CASCADE;
+CREATE TABLE IF NOT EXISTS photos 
+(
+	photouuid uuid DEFAULT uuid_generate_v4 ()
+	--,project varchar (50) NOT NULL -- Removing: this only belongs in the project file and will be joined using a foreign key
+	,projectid INT
+	,survey_run text NOT NULL
+	,photo_filename text NOT NULL
+	,photo BYTEA NOT NULL
+	,thumbnail BYTEA
+	,deviceid BIGINT
+	,PRIMARY KEY(photouuid)
+	,CONSTRAINT fk_project -- add foreign key constraint referencing the project ID
+         FOREIGN KEY(projectid) 
+    	   REFERENCES projects(projectid)
+	   ON DELETE CASCADE
+	,CONSTRAINT fk_devicephoto -- add foreign key constraint referencing the project ID
+        FOREIGN KEY(deviceid) 
+            REFERENCES devices(deviceid)
+        ON DELETE CASCADE
+);
+
+
