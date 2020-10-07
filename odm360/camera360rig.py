13f753434236c6e9c064bd8e49b73b62185af376
hcwinsemius <h.c.winsemius@gmail.com>
2020-10-07 18:06:42 +0200

    camerarig some slight cleaning #63

--- odm360/camera360rig.py
+++ odm360/camera360rig.py
@@ -1,7 +1,6 @@
 import logging
 import time
-from flask import request, current_app
-import numpy as np
+from flask import request
 import datetime
 from odm360 import dbase, utils
 from odm360.states import states
@@ -28,15 +27,12 @@ def do_request(cur, method='GET'):
     msg = request.get_json()
     # Create or update state of current camera
     state = msg['state']
-    # device_uuid = msg['device_uuid']  # TODO: change this into the uuid of the device, once modified on the child side database setup
-
-    # device_ip = msg['ip']   # TODO: change this into the uuid of the device, once modified on the child side database setup
     # check if the device exists.
     if dbase.is_device(cur, state['device_uuid']):
         dbase.update_device(cur,
                             state['device_uuid'],
                             states[state['status']]
-                            )  # TODO: add last_photo in the form of a thumbnail, requires modification of dbase last_photo data type
+                            )
     else:
         dbase.insert_device(cur,
                             state['device_uuid'],
@@ -92,12 +88,10 @@ def get_task(cur, state):
     # TODO: remove the automatic stopping after 10 secs
     rig = dbase.query_project_active(cur, as_dict=True)
 
-    cur_address = request.remote_addr
     cur_device = dbase.query_devices(cur, device_uuid=state['device_uuid'], as_dict=True, flatten=True)
     # get states of parent and child in human readable format
     device_status = utils.get_key_state(cur_device['status'])
     rig_status = utils.get_key_state(rig['status'])
-    # print(f'DEVICE STATE IS {device_status} and RIG STATE IS {rig_status}')
 
     if device_status != rig_status:
         # something needs to be done to get the states the same
@@ -153,7 +147,6 @@ def activate_camera(cur, state):
     # check how many cams have the state 'ready', only start when the full rig is ready
     n_cams_ready = len(dbase.query_devices(cur, status=states['ready']))
 
-
     # compute cams ready from a PostGreSQL query
     if n_cams_ready == project['n_cams']:
         logger.info(f'All cameras ready. Start capturing on device {state["device_uuid"]} on ip {state["ip"]}')
@@ -174,10 +167,10 @@ def activate_camera(cur, state):
                            'project': project},
                 }
     else:
+        # this should not happen as the front end part already checks if enough cams are online.
         logger.info(f'Only {n_cams_ready} out of {project["n_cams"]} ready for capture, switching state back')
         return {'task': 'wait',
                 'kwargs': {}
                 }
         # roll back to state "ready"
         dbase.update_project_active(cur, status=states['ready'])
-    # TODO check wait
