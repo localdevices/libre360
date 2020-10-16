8962a1626e547bfcf9be9a1a84545cf4d92a5507
hcwinsemius <h.c.winsemius@gmail.com>
2020-10-16 14:47:46 +0100

    modified photos_child to a normal table

--- provisioning/dbase_child.sql
+++ provisioning/dbase_child.sql
@@ -1,6 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS postgres_fdw;
-CREATE EXTENSION IF NOT EXISTS "multicorn";
+-- CREATE EXTENSION IF NOT EXISTS "multicorn";
 
 -- drop any tables if they exist
 DROP TABLE IF EXISTS device;
@@ -15,29 +15,25 @@ CREATE TABLE IF NOT EXISTS device
 );
 
 -- add file system foreign data wrapper
-CREATE SERVER filesystem_srv foreign data wrapper multicorn options (
-    wrapper 'multicorn.fsfdw.FilesystemFdw'
-);
+--CREATE SERVER filesystem_srv foreign data wrapper multicorn options (
+--    wrapper 'multicorn.fsfdw.FilesystemFdw'
+--);
 
 -- create photos table (dependent on projects)
-CREATE FOREIGN TABLE photos_child (
-    device_uuid UUID
-    ,project_id INT
+CREATE TABLE photos_child (
+    photo_uuid uuid
+    ,project_id int
     ,survey_run text
+    ,device_uuid UUID
+    ,device_name text
     ,photo_filename text
     ,photo bytea
-    ,filename character varying
-) server filesystem_srv options(
-    root_dir    '/home/pi/piimages'
-    ,pattern '{device_uuid}/{project_id}/{survey_run}/{photo_filename}.jpg'
-    ,content_column 'photo'
-    ,filename_column 'filename'
 );
-
-
 ALTER TABLE device OWNER TO odm360;
 ALTER TABLE photos_child OWNER TO odm360;
 -- Now add one device
 INSERT INTO device (name) VALUES ('picam');
 -- And that's it! Only one device entry ever!
+-- make odm360 superuser so that pg_read_binary_file can be used (ugh! change with psycopg3 if it comes available)
+ALTER USER odm360 WITH SUPERUSER;
 
