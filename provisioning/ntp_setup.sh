f8165b47ebb57a011f761349377e9d0d39796aa2
ivangayton <ivangayton@gmail.com>
2020-11-04 17:32:37 +0900

    NTP install permissions and typo #37

--- provisioning/ntp_setup.sh
+++ provisioning/ntp_setup.sh
@@ -8,8 +8,8 @@ sudo systemctl stop systemd-timesyncd
 sudo systemctl disable systemd-timesyncd
 
 echo Starting the full NTP server
-sudo /etc/init.d ntp stop
-sudo /etc/init.d ntp start
+sudo /etc/init.d/ntp stop
+sudo /etc/init.d/ntp start
 
 echo at this point the setup depends whether this device is the parent (which will be the NTP server) or a child (which will be an NTP client)
 
