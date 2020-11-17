ca108d327ff073be8b152f6c73b7d92ad102a0ce
Stephen Mather <1174901+smathermather@users.noreply.github.com>
2020-11-17 09:34:47 -0500

    Add parent name, fixes #139

--- parent_pi_setup.sh
+++ parent_pi_setup.sh
@@ -50,6 +50,9 @@ echo starting and enabling the odm360dashboard service with Systemd
 sudo systemctl start odm360dashboard.service
 sudo systemctl enable odm360dashboard.service
 
+echo Naming the parent
+echo parent.local | sudo tee -a /etc/hostname
+
 echo "************************************"
 echo Now you should have a $model set up as a Parent for an ODM360 rig.
 echo "************************************"
