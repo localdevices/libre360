81f400e4190df1de1c0cf25458b674aeca3174fa
Stephen V. Mather <smathermather@protonmail.com>
2020-12-15 11:44:51 -0500

    add some echo statements and apply hostname update manually

--- child_pi_setup_rename.sh
+++ child_pi_setup_rename.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # retrieve the child's old uuid and change the hostname of child accordingly
+echo 'Retrieving current hostname...'
 old_hostname_prefix=`sudo -u postgres psql -d odm360 -t -c "SELECT (device_uuid) FROM device;"`
 
+echo 'Removing existing device name from database and generating new name...'
 # Remove existing device records and generate a new ID
 sudo -u postgres psql -d odm360 -t -c "TRUNCATE TABLE device;"
 sudo -u postgres psql -d odm360 -t -c "INSERT INTO device (name) VALUES ('picam');"
 hostname_prefix=`sudo -u postgres psql -d odm360 -t -c "SELECT (device_uuid) FROM device;"`
 
+echo 'Assigning new hostname to device'
 # Apply new ID to hosts
 echo $hostname_prefix | sudo tee /etc/hostname
 sudo sed -i "s/$old_hostname_prefix/$hostname_prefix/g" /etc/hosts
+hostname $hostname_prefix
 
