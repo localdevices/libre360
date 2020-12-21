323b69ae1b2e330086923d261955aaf58cc1567f
hcwinsemius <h.c.winsemius@gmail.com>
2020-12-21 15:31:55 +0100

    adding ttyACM0 as device to gpsd config #105

--- provisioning/gpsd_setup.sh
+++ provisioning/gpsd_setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo Installing GPSD
-sudo apt install -y gpsd gpsd-clients python-gpsd pps-tools
+sudo apt install -y gpsd gpsd-clients pps-tools
 
 # echo Setting GPSD to listen to the GNSS on the serial port instead of USB
 # sudo sed -i 's/USBAUTO="true"/USBAUTO="false"/g' /etc/default/gpsd
-# sudo sed -i 's:DEVICES="":DEVICES="/dev/ttyS0 /dev/pps0":g' /etc/default/gpsd
+sudo sed -i 's:DEVICES="":DEVICES="/dev/ttyACM0 /dev/pps0":g' /etc/default/gpsd
 # sudo sed -i 's:GPSD_OPTIONS="":GPSD_OPTIONS="-n":g' /etc/default/gpsd
 
 echo Enabling a GPIO pin to receive the PPS signal
