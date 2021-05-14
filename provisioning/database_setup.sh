b8dfb5625ec15213d388cd53b6ef62894ff6ac0d
hcwinsemius <h.c.winsemius@gmail.com>
2021-05-14 11:36:23 +0200

    modified some naming to libre360 #173

--- provisioning/database_setup.sh
+++ provisioning/database_setup.sh
@@ -5,23 +5,20 @@
 echo Installing posgresql
 sudo apt install -y postgresql postgresql-contrib libpq-dev  >> provisioning/setup.log 2>> provisioning/error.log
 
-echo Creating postgres user odm360
+echo Creating postgres user libre360
 # TODO This is a pretty obvious password (it's md5 hashed).
 # Generate another one using bash:
 # echo -n passwordusername | md5sum
 # and paste the result into the following command
 # with the prefix 'md5' (the hash below begins with 44ec)
-sudo -u postgres psql -c "CREATE ROLE odm360 WITH PASSWORD 'md544ec1a1a609f26f78f20decd3a34bd2d';"
-sudo -u postgres psql -c "ALTER ROLE odm360 WITH LOGIN"
-sudo -u postgres psql -c "ALTER ROLE odm360 WITH SUPERUSER"
+sudo -u postgres psql -c "CREATE ROLE libre360 WITH PASSWORD 'md5199464a7f2da85a3f9607c40864718c6';"
+sudo -u postgres psql -c "ALTER ROLE libre360 WITH LOGIN"
+sudo -u postgres psql -c "ALTER ROLE libre360 WITH SUPERUSER"
 
-echo Creating database odm360
-sudo -u postgres psql -c "CREATE DATABASE odm360 WITH OWNER odm360;"
-
-echo Adding extensions and tables to database odm360
-sudo -u postgres psql -d odm360 -f provisioning/dbase.sql
+echo Creating database libre360
+sudo -u postgres psql -c "CREATE DATABASE libre360 WITH OWNER libre360;"
 
 echo ##########################################################
-echo Now you should have a Postgresql database with a user and password properly configured to connect to using psycopg2 from Python.
+echo Now you should have a Postgresql database with a user and password properly configured to connect to using SQLAlchemy from Python.
 echo ##########################################################
 echo
